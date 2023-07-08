c8bf4c5150e86779fe2dfebea17b1b316444113b
dimitris <iracleous@gmail.com>
2023-07-09 00:01:31 +0300

    Add project files.

---
+++ HumanResources/HrDbContenxts/HrDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HumanResources.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HumanResources.HrDbContenxts
+{
+    public class HrDbContext:DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+
+        public HrDbContext()
+        {
+
+        }
+
+        public HrDbContext(DbContextOptions<HrDbContext> options)
+       : base(options)
+        {
+        }
+
+    }
+}
---
+++ HumanResources/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace HumanResources.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public decimal Salary    { get; set; }
+        public DateTime HiringDate { get; set; }
+
+    }
+}
